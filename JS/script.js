aa222043e63e2152339bd5e91fe3c8b88597cc72
JUSEAN <130252028+JUSEAN@users.noreply.github.com>
2023-04-21 17:58:46 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,10 @@ function encrypt() {
   var input = document.getElementById("input").value;
   var password = document.getElementById("password").value;
 
+  // Verifica si el input y la contraseña no están en blanco
   if (input !== "" && password !== "") {
     var ciphertext = CryptoJS.AES.encrypt(input, password).toString();
+    ciphertext = "JUSEAN" + ciphertext + ".jsn"; // agrega "JUSEAN" al principio y ".jsn" al final
     document.getElementById("output").value = ciphertext;
   } else {
     alert("Por favor, ingrese texto y una key.");
@@ -14,13 +16,13 @@ function decrypt() {
   var ciphertext = document.getElementById("input").value;
   var password = document.getElementById("password").value;
 
+  // Verifica si el input y la contraseña no están en blanco
   if (ciphertext !== "" && password !== "") {
-    var bytes = CryptoJS.AES.decrypt(ciphertext, password);
+    // Remueve "JUSEAN" al principio y ".jsn" al final antes de desencriptar
+    var bytes = CryptoJS.AES.decrypt(ciphertext.slice(6, -4), password);
     var plaintext = bytes.toString(CryptoJS.enc.Utf8);
     document.getElementById("output").value = plaintext;
   } else {
     alert("Por favor, ingrese texto cifrado y una key.");
   }
 }
-
-  
